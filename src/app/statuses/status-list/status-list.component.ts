800ce4597b5460347243f28c1630997e547cddc3
patrick <patrickraspante@gmail.com>
2018-03-17 11:55:30 -0700

    fixing some compilation issues

--- src/app/statuses/status-list/status-list.component.ts
+++ src/app/statuses/status-list/status-list.component.ts
@@ -47,8 +47,8 @@ export class StatusListComponent implements OnInit {
   createNewStatus() {
     var status: Status = {
       title: '',
-      //creation: '',
-      //last_update: '',
+      creation: new Date(Date.now()).toISOString(),
+      last_update: new Date(Date.now()).toISOString(),
       text: ''
     };
 
